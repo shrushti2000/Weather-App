5ce54b271c9b818e321912fb334d83b4858b06f4
shrushti2000 <shrushtipolekar@gmail.com>
2020-05-30 19:37:10 +0530

    Updates

---
+++ precache-manifest.df15935f6fff1e6c34740cfef29e7195.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02a323f180ed9ac1761aa3c4c800886d",
+    "url": "/Weather-App/index.html"
+  },
+  {
+    "revision": "0fa739da5afd091843ad",
+    "url": "/Weather-App/static/css/main.b54f09fb.chunk.css"
+  },
+  {
+    "revision": "820f288b32e90f743e06",
+    "url": "/Weather-App/static/js/2.d9b2bb25.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather-App/static/js/2.d9b2bb25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fa739da5afd091843ad",
+    "url": "/Weather-App/static/js/main.f7f92140.chunk.js"
+  },
+  {
+    "revision": "a4ccdbad56bedd45b82e",
+    "url": "/Weather-App/static/js/runtime-main.f92ac621.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Weather-App/static/media/logo.5d5d9eef.svg"
+  }
+]);
